295fb2d67816e75e77a984337c8595e364643144
Joshua Kwiatkowski <jck2957@rit.edu>
2015-02-13 09:51:00 -0500

    Added a game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public int monastaries;
+	public int barbarians;
+	public int knights;
+	public int monks;
+
+	//Barbarian[] barray;
+	//Knight[] karray;
+	//Monk[] marray
+
+	// Use this for initialization
+	void Start () {
+		Initialize ();
+	}
+
+	void Initialize() {
+		for (int i = 0; i < monastaries; i++) {
+			//Create monastaries
+		}
+		for (int i = 0; i < barbarians; i++) {
+			//Create barbarians at random locations
+		}
+		for (int i = 0; i < knights; i++) {
+			//Create knights at random locations
+		}
+		for (int i = 0; i < monks; i++) {
+			//Create monks at random locations
+		}
+	}
+}
