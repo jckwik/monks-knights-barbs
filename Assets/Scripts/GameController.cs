7591f1ef1033cc045072f718603725e0d073143a
Joshua Kwiatkowski <jck2957@rit.edu>
2015-02-16 14:51:17 -0500

    Added a movableObject script, trying to get prefabs to spawn. Almost there but running into a few issues.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
@@ -8,9 +9,13 @@ public class GameController : MonoBehaviour {
 	public int knights;
 	public int monks;
 
-	//Barbarian[] barray;
-	//Knight[] karray;
-	//Monk[] marray
+	public MovableObject barbarianFab;
+	public MovableObject knightFab;
+	public MovableObject monkFab;
+
+	List<MovableObject> barray = new List<MovableObject> ();
+	List<MovableObject> karray = new List<MovableObject> ();
+	List<MovableObject> marray = new List<MovableObject> ();
 
 	// Use this for initialization
 	void Start () {
@@ -18,16 +23,23 @@ public class GameController : MonoBehaviour {
 	}
 
 	void Initialize() {
+
 		for (int i = 0; i < monastaries; i++) {
 			//Create monastaries
 		}
 		for (int i = 0; i < barbarians; i++) {
 			//Create barbarians at random locations
+			Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+			barray.Add(Instantiate(barbarianFab, pos, Quaternion.identity) as MovableObject);
 		}
 		for (int i = 0; i < knights; i++) {
 			//Create knights at random locations
+			Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+			karray.Add(Instantiate(knightFab, pos, Quaternion.identity) as MovableObject);
 		}
 		for (int i = 0; i < monks; i++) {
+			Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+			marray.Add(Instantiate(monkFab, pos, Quaternion.identity) as MovableObject);
 			//Create monks at random locations
 		}
 	}
