21ebfb91b05bd266e2bb9018b2a765f06ac96d84
PinkPotatoes <dxl2911@rit.edu>
2015-02-16 23:41:08 -0500

    Seek and Flee

---
+++ Assets/Scripts/Flee.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flee : MonoBehaviour {
+	public float moveSpeed;
+	public float rotationSpeed;
+	public GameObject target;
+	public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		rotationSpeed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = this.transform.position - target.transform.position;
+		this.transform.LookAt(target.transform, Vector3.up);
+		this.transform.Translate(Vector3.forward * -moveSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Seek.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seek : MonoBehaviour {
+	public float moveSpeed;
+	public float rotationSpeed;
+	public GameObject target;
+	public Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		rotationSpeed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = target.transform.position - this.transform.position;
+		this.transform.LookAt(target.transform, Vector3.up);
+		this.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+	}
+}
