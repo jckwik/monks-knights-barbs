43fcee3f9685b2e4c31c4bba1cc9be210c57b06f
PinkPotatoes <dxl2911@rit.edu>
2015-02-26 23:16:26 -0500

    Added non-functional path follow

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,10 +16,10 @@ public class GameController : MonoBehaviour {
 	public GameObject playerFab;
 	public GameObject monastaryFab;
 
-	List<GameObject> barray = new List<GameObject> ();
-	List<GameObject> karray = new List<GameObject> ();
-	List<GameObject> marray = new List<GameObject> ();
-	List<GameObject> monarray = new List<GameObject> ();
+	public List<GameObject> barray = new List<GameObject> ();
+	public List<GameObject> karray = new List<GameObject> ();
+	public List<GameObject> marray = new List<GameObject> ();
+	public List<GameObject> monarray = new List<GameObject> ();
 
 	// Use this for initialization
 	void Start () {
@@ -32,6 +32,15 @@ public class GameController : MonoBehaviour {
 		for (int i = 0; i < monastaries; i++) {
 			//Create monastaries
 		}
+		GameObject mon = (GameObject)Instantiate (monastaryFab, new Vector3(-20, 1, -20), Quaternion.identity);
+		monarray.Add (mon);
+		mon = (GameObject)Instantiate (monastaryFab, new Vector3(20, 1, -20), Quaternion.identity);
+		monarray.Add (mon);
+		mon = (GameObject)Instantiate (monastaryFab, new Vector3(20, 1, 20), Quaternion.identity);
+		monarray.Add (mon);
+		mon = (GameObject)Instantiate (monastaryFab, new Vector3(-20, 1, 20), Quaternion.identity);
+		monarray.Add (mon);
+		
 		for (int i = 0; i < barbarians; i++) {
 			//Create barbarians at random locations
 			Vector3 pos = new Vector3(Random.Range(-10.0f, 10.0f), 1, Random.Range(-10.0f, 10.0f));
@@ -95,5 +104,72 @@ public class GameController : MonoBehaviour {
 		Vector3 target = circleLoc + circleOffSet;
 
 		return Seek( pos, target, speed);	
+	}
+	
+	public Vector3 PathFollow(List<GameObject> path, Vector3 pos, float speed, Vector3 direction) {
+		int curWP = -1;
+		float pathRadius = 5;
+		// Predict the movement
+		Vector3 predict = direction.normalized * speed * 2;
+		Vector3 predictLoc = predict + pos;
+		
+		// Find noraml to the path from the predicted location;
+		Vector3 normal;
+		Vector3 target = Vector3.zero;
+		float worldRecord = 10000000;
+		
+		// Loop through all the points in the path;
+		int pathSize = path.Count;
+		for (int i=0; i < pathSize; i++) {
+			// Look at a line segment
+			Vector3 a = path[i%pathSize].transform.position;
+			Vector3 b = path[(i+1)%pathSize].transform.position;
+			// Get the normal point to that line
+			Vector3 normalPoint = GetNormal(predictLoc, a, b);
+
+			// Chec if normal is on line segment
+			Vector3 dir = b -a;
+			//Fi it's not within the line segment, consider the normal to just be the end of the line segment (point b)
+			// if (da + db > line.mag() +1)
+			if (normalPoint.x < Mathf.Min(a.x, b.x) || normalPoint.x > Mathf.Max(a.x, b.x) ||
+			    normalPoint.y < Mathf.Min(a.y, b.y) || normalPoint.y > Mathf.Max(a.y, b.y)) {
+
+				normalPoint = b + Vector3.zero;
+				// if we're at the end we really want the next line segment for looking ahead
+				a = (Vector3)(path[(i+1)%pathSize].transform.position);
+				b = (Vector3)(path[(i+2)%pathSize].transform.position);
+				dir = b - a;
+			}
+
+			// How far away are we from the path?
+			Vector3 dVec = predictLoc - normalPoint;
+			float d = dVec.magnitude;
+			// Did we beat the worldRecord and find the closest line segment?
+			if (d<worldRecord) {
+				normal = normalPoint;
+				curWP = i % pathSize;
+
+				// Look at the direction of the line segment so we can seek a little bit ahead of the normal
+				dir = dir.normalized;
+				dir *= speed;
+				target = Vector3.zero + normal; // **NOT SURE IF THIS WORKS **
+				target += dir;
+			}
+		}
+		
+		if (worldRecord > pathRadius) {
+			return Seek (pos, target, speed);
+		}
+		else {
+			return direction.normalized * speed;
+		}
+	}
+
+	public Vector3 GetNormal(Vector3 a, Vector3 b, Vector3 c) {
+		Vector3 side1 = b-a;
+		Vector3 side2 = c-a;
+
+		// Cross the vectors to get a perpendicular vector, then normalize it.
+		return Vector3.Cross(side1, side2).normalized;
 	}
 }
