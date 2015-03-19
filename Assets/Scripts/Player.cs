21ec24966bfcbbb0ab3ca2b967ccfecb7ab14ad5
Joshua Kwiatkowski <jck2957@rit.edu>
2015-03-19 15:25:16 -0400

    Changes to how the player moves and how it's created.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,10 +7,15 @@ public class Player : MonoBehaviour {
 	public KeyCode moveBackward = KeyCode.S;
 	public KeyCode moveLeft = KeyCode.A;
 	public KeyCode moveRight = KeyCode.D;
+	public KeyCode turnLeft = KeyCode.LeftArrow;
+	public KeyCode turnRight = KeyCode.RightArrow;
 
 	public float speed = 5.0f;
+	public float turnSpeed = 20.0f;
 	public Vector3 direction;
-	
+
+	Camera cam;
+
 	//public GameObject mainCam;
 
 	Vector2 location;
@@ -22,33 +27,45 @@ public class Player : MonoBehaviour {
 		location = Vector2.zero;
 		velocity = Vector2.zero;
 		acceleration = Vector2.zero;
-		//mainCam = GameObject.FindGameObjectWithTag("MainCamera");
+
+		cam = Camera.main;
+		direction = new Vector3 (1, 0, 0);
+		//cam = (Camera)GameObject.FindGameObjectWithTag("MainCamera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//float velX = 0.0f;
 		//float velZ = 0.0f;
-		direction = this.transform.forward;
+		//direction = new Vector3(this.transform.rotation.y, 0, this.transform.rotation.w);
 		
 		// Check for up and down movement, else float
 		if(Input.GetKey(moveForward)){
 
-			this.transform.Translate(direction * speed * Time.deltaTime);
+			this.transform.Translate(this.transform.forward * speed * Time.deltaTime, cam.transform);
 		}
 		else if(Input.GetKey(moveBackward)){
-			this.transform.Translate(direction * -1f * speed * Time.deltaTime);
+			this.transform.Translate(this.transform.forward * -1f * speed * Time.deltaTime, cam.transform);
 		}
 		
 		// Check for left and right movement
 		if(Input.GetKey(moveRight)){
-			this.transform.Translate(Vector3.Cross(-direction, Vector3.up) * speed * Time.deltaTime);
+			this.transform.Translate(Vector3.Cross(-this.transform.forward, Vector3.up) * speed * Time.deltaTime, cam.transform);
 		}
 		else if(Input.GetKey(moveLeft)){
-			this.transform.Translate(Vector3.Cross(direction, Vector3.up) * speed * Time.deltaTime);;
+			this.transform.Translate(Vector3.Cross(this.transform.forward, Vector3.up) * speed * Time.deltaTime, cam.transform);
 		}
 
-		this.transform.LookAt(direction);
+		if (Input.GetKey (turnLeft)) {
+			this.transform.Rotate (Vector3.down * Time.deltaTime * turnSpeed);
+			Debug.Log("Turning Left");
+		}
+		else if (Input.GetKey (turnRight)) {
+			this.transform.Rotate (Vector3.up * Time.deltaTime * turnSpeed);
+			Debug.Log("Turning Right");
+		}
+
+		//this.transform.LookAt(direction);
 		this.transform.position = new Vector3(this.transform.position.x, 1, this.transform.position.z);
 		this.transform.rotation = new Quaternion(0, this.transform.rotation.y, 0, this.transform.rotation.w);
 		//location += velocity;
