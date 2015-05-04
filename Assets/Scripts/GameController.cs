b93c5bef783348cab6232f630d48f48b46076efe
unknown <sas8463@GDD-19.main.ad.rit.edu>
2015-05-04 09:52:32 -0400

    More GA!

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -31,6 +31,7 @@ public class GameController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		roundNumber = 0;
+		LoadData();
 		Initialize ();
 	}
 
@@ -104,7 +105,70 @@ public class GameController : MonoBehaviour {
 
 		}
 	}
+	/*
+	void ProcessData()
+	{
+		for (int i = 0; i < barbarians; i++) {
+			Individual p1 = Select ();					// Get 2 parents
+			Individual p2 = Select ();
+			uint c1 = p1.Chrom;							// Extract their chromosomes
+			uint c2 = p2.Chrom;
+			
+			if (Util.rand.NextDouble () < CROSSOVER_PROB) // Probably do crossover
+			{
+				uint kidChrom = CrossOver (c1, c2);		// Make new chromosome
+				Individual newDude = new Individual (kidChrom, nBits); // Make Individual
+				newDude.Mutate ();						// Maybe mutate a bit
+				return newDude;							// Send it back
+			}
+			else
+				// No crossover => Pick one of the parents to return unchanged
+				return (Util.rand.NextDouble() < 0.5 ? p1 : p2);
+		}
+		for (int i = 0; i < knights; i++) {
+			GameObject p1 = karray[Random.Range(0,knights)];				// Get 2 parents
+			GameObject p2 = karray[Random.Range(0,knights)];
+			uint c1 = p1.GetComponent<KnightScript>().chrom;							// Extract their chromosomes
+			uint c2 = p2.GetComponent<KnightScript>().chrom;
+			
+			if (Random.Range(0,100)/100.0 < 0.9) // Probably do crossover
+			{
+				int xOverPt = Util.rand.Next (0, nBits);	// Pick random crossover point
+				p1 = (p1 >> xOverPt) << xOverPt;			// Get p1's bits to the left
+				p2 = (p2 << (32 - xOverPt)) >> (32 - xOverPt); // p2's to the right
+				uint newKidChrom = p1 | p2;						// Or them together// Make new chromosome
+				chroms.Add (newKidChrom);
 
+				//Individual newDude = new Individual (kidChrom, nBits); // Make Individual
+				//newDude.Mutate ();						// Maybe mutate a bit
+				GameObject pResult = (Random.Range(0,100)/100.0 < 0.5 ? p1 : p2);						// Send it back
+			}
+			else
+			{
+				// No crossover => Pick one of the parents to return unchanged
+				GameObject pResult = (Random.Range(0,100)/100.0 < 0.5 ? p1 : p2);
+				//fitnessValues.Add(
+			}
+		}
+		for (int i = 0; i < monks; i++) {
+			Individual p1 = Select ();					// Get 2 parents
+			Individual p2 = Select ();
+			uint c1 = p1.Chrom;							// Extract their chromosomes
+			uint c2 = p2.Chrom;
+			
+			if (Util.rand.NextDouble () < CROSSOVER_PROB) // Probably do crossover
+			{
+				uint kidChrom = CrossOver (c1, c2);		// Make new chromosome
+				Individual newDude = new Individual (kidChrom, nBits); // Make Individual
+				newDude.Mutate ();						// Maybe mutate a bit
+				return newDude;							// Send it back
+			}
+			else
+				// No crossover => Pick one of the parents to return unchanged
+				return (Util.rand.NextDouble() < 0.5 ? p1 : p2);
+		}
+	}
+	*/
 	void StoreData()
 	{
 		StreamWriter outStream = new StreamWriter ("fitnessValues.txt",false);
@@ -162,8 +226,14 @@ public class GameController : MonoBehaviour {
 			//Create barbarians at random locations
 			Vector3 pos = new Vector3(Random.Range(-225.0f, 225.0f), 1, Random.Range(-225.0f, 225.0f));
 			GameObject barb = (GameObject)Instantiate(barbarianFab, pos, Quaternion.identity);
-			barb.GetComponent<BarbarianScript>().fitnessValue = fitnessValues[creationCount];
-			barb.GetComponent<BarbarianScript>().chrom = chroms[creationCount];
+			try{
+				barb.GetComponent<BarbarianScript>().fitnessValue = fitnessValues[creationCount];
+				barb.GetComponent<BarbarianScript>().chrom = chroms[creationCount];
+			}
+			catch{
+				barb.GetComponent<BarbarianScript>().fitnessValue = 10;
+				barb.GetComponent<BarbarianScript>().chrom = 10;
+			}
 			barray.Add(barb);
 			creationCount++;
 		}
@@ -171,8 +241,15 @@ public class GameController : MonoBehaviour {
 			//Create knights at random locations
 			Vector3 pos = new Vector3(Random.Range(-225.0f, 225.0f), 1, Random.Range(-225.0f, 225.0f));
 			GameObject knight = (GameObject)Instantiate(knightFab, pos, Quaternion.identity);
-			knight.GetComponent<KnightScript>().fitnessValue = fitnessValues[creationCount];
-			knight.GetComponent<KnightScript>().chrom = chroms[creationCount];
+			try
+			{
+				knight.GetComponent<KnightScript>().fitnessValue = fitnessValues[creationCount];
+				knight.GetComponent<KnightScript>().chrom = chroms[creationCount];
+			}
+			catch{
+				knight.GetComponent<KnightScript>().fitnessValue = 10;
+				knight.GetComponent<KnightScript>().chrom = 10;
+			}
 			karray.Add(knight);
 			creationCount++;
 		}
@@ -180,8 +257,15 @@ public class GameController : MonoBehaviour {
 			//Create monks at random locations
 			Vector3 pos = new Vector3(Random.Range(-225.0f, 225.0f), 1, Random.Range(-225.0f, 225.0f));
 			GameObject monk = (GameObject)Instantiate(monkFab, pos, Quaternion.identity);
-			monk.GetComponent<MonkScript>().fitnessValue = fitnessValues[creationCount];
-			monk.GetComponent<MonkScript>().chrom = chroms[creationCount];
+			try
+			{
+				monk.GetComponent<MonkScript>().fitnessValue = fitnessValues[creationCount];
+				monk.GetComponent<MonkScript>().chrom = chroms[creationCount];
+			}
+			catch{
+				monk.GetComponent<MonkScript>().fitnessValue = 10;
+				monk.GetComponent<MonkScript>().chrom = 10;
+			}
 			marray.Add(monk);
 			creationCount++;
 		}
